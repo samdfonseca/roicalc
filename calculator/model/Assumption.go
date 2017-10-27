8248f32e48908f39d6aaf5ae5ec214a308edc939
Sam Fonseca <samfonseca@utexas.edu>
2017-10-27 12:45:06 -0400

    added json annotations

--- calculator/model/Assumption.go
+++ calculator/model/Assumption.go
@@ -1,20 +1,18 @@
 package model
 
 type Assumption struct {
-	Aov                       float64
-	PurchasePerYear           float64
-	PointPerDollarSpend       float64
-	DollarValuePerPoint       float64
-	DiscountToRewards         float64
-	AnnualPerMemberMarketCost float64
-	MembershipStartingLevel   float64
-	MembershipGrowth          float64
-	PurchasingMembers         float64
-	EngagementPointsPerMember float64
-	LiftToSpend               float64
-	Redemption                float64
-	PointExpiryRate           float64
-	ProgramCostsFirstYear     float64
-	ProgramCostsSecondYear    float64
-	ProgramCostThirdYear      float64
+	Aov                       float64   `json:"aov"`
+	PurchasePerYear           float64   `json:"purchase_per_year"`
+	PointPerDollarSpend       float64   `json:"points_per_spend"`
+	DollarValuePerPoint       float64   `json:"dollar_point_value"`
+	DiscountToRewards         float64   `json:"discount_to_rewards"`
+	AnnualPerMemberMarketCost float64   `json:"member_marketing_cost"`
+	MembershipStartingLevel   float64   `json:"starting_members"`
+	MembershipGrowth          float64   `json:"membership_growth"`
+	PurchasingMembers         float64   `json:"purchasing_members"`
+	EngagementPointsPerMember float64   `json:"engagement_points"`
+	LiftToSpend               float64   `json:"lift_to_spend"`
+	Redemption                float64   `json:"redemption_rate"`
+	PointExpiryRate           float64   `json:"point_expiry_rate"`
+	ProgramCosts              []float64 `json:"program_costs"`
 }
